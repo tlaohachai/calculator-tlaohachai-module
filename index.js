f8f3e61295a55644a3cd359650a0655278c82852
Tanaporn Laohachai [TH] <TLaohachai@kerrylogistics.com>
2019-07-25 11:59:05 +0700

    เพิ่มฟังก์ชันหาร

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 module.exports = {
     plus: (first, second) => first + second,
     subtract: (first, second) => first - second,
-    multiply: (first, second) => first * second
+    multiply: (first, second) => first * second,
+    divide: (first, second) => first / second
 }
